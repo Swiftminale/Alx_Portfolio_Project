d277099b54301aed2d028f504399937d61da7575
Swiftminale <minalefetene@gmail.com>
2024-10-08 14:52:02 +0300

    +ed authentification strategies with JWT

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-// import {Role} from 'src/auth/enums/role.enum'
+import {Role} from 'src/auth/enums/role.enum'
 export type UserDocument = User & Document;
 
 @Schema()
@@ -14,8 +14,8 @@ export class User {
   @Prop()
   password: string;
 
-  //   @Prop()
-  //   roles: Role[];
+    @Prop()
+    roles: Role[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
